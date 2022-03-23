b4bbf2176204521df6d0e207cc251423001bb170
Altify <Altify@mail.com>
2022-03-23 11:01:39 -0700

    v0.2.3
    msginfo

--- source/gmail_generator.py
+++ source/gmail_generator.py
@@ -12,3 +12,20 @@ import sys
 import time
 import random
 import string
+
+# Printing funtion with 3 modes
+# 1 : Normal message
+# 2 : Information message
+# 3 : Caution message
+def msg(
+        _option_,
+        _message_
+        ):
+    if _option_ == 1:
+        print('\x1b[0;32;40m> %s\x1b[0m' % _message_)
+    elif _option_ == 2:
+        print('\x1b[0;32;40m>\x1b[0m %s' % _message_)
+    elif _option_ == 3:
+        print('\n\x1b[0;32;40m[\x1b[0m%s\x1b[0;32;40m]\x1b[0m' % _message_)
+    else:
+        print('\n\x1b[0;31;40m[ERROR]\x1b[0m')
