1e6eca8f2aee1be451ba98e9e811e34658a3177b
Altify <Altify@mail.com>
2022-03-23 10:59:45 -0700

    v0.0.1
    Sourcing and credits.

---
+++ source/gmail_generator.py
@@ -0,0 +1,8 @@
+#! python3
+
+#   Author      : Altify
+#   Updated    : blactorio
+#   GitHub      : https://github.com/Altify-Development
+#   GitHub      : https://github.com/Altify-Developing
+#   Year        : 2022
+#   Description : [Updated] Script that generates random Gmail account. Still stalls at phone verification.
